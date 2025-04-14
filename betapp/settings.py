4c13b842395406db77b1ce37cde25a0433041168
Furkan Yılmaz <furkanyilmaz@192.168.1.115>
2025-04-14 18:46:18 +0300

    abc

--- betapp/settings.py
+++ betapp/settings.py
@@ -174,7 +174,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'furkanyl509@gmail.com'
-EMAIL_HOST_PASSWORD = 'obm acjm sqbv mxwu'
+EMAIL_HOST_PASSWORD = 'xnqp npsg orua wljz'
